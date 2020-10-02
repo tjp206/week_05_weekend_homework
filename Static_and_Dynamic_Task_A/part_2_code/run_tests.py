bf6ca265f1d0837b109076bf65fd29afdf02af6d
TJ <tjpataria@TJs-MacBook-Pro.local>
2020-10-02 12:53:28 +0100

    finished part 2 - dynamic testing & added screenshots

--- Static_and_Dynamic_Task_A/part_2_code/run_tests.py
+++ Static_and_Dynamic_Task_A/part_2_code/run_tests.py
@@ -3,3 +3,4 @@ from tests.card_game_tests import TestCardGame
 
 if __name__ == "__main__":
     unittest.main()
+
--- Static_and_Dynamic_Task_A/part_2_code/tests/card_game_tests.py
+++ Static_and_Dynamic_Task_A/part_2_code/tests/card_game_tests.py
@@ -2,5 +2,21 @@ import unittest
 from src.card import Card
 from src.card_game import CardGame
 
-class TestCardGame:
-    pass
+class TestCardGame(unittest.TestCase):
+
+    def setUp(self):
+        self.card_ace = Card('spades', 1)
+        self.card1 = Card('hearts', 10)
+        self.card2 = Card('clubs', 5)
+        self.card_game = CardGame()
+
+    def test_check_for_ace(self):
+        self.assertEquals(True, self.card_game.check_for_ace(self.card_ace))
+
+    def test_highest_card(self):
+        self.assertEquals(self.card1, self.card_game.highest_card(self.card1, self.card2))
+
+    def test_cards_total(self):
+        cards = [self.card1, self.card2]
+        self.assertEquals("You have a total of 15", self.card_game.cards_total(cards))
+
